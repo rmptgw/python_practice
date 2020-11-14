4f8624c5dc8c7bc6ca69ea13130b2fabc4ff8c24
rmptgw <rmptgw@gmail.com>
2020-11-14 17:00:15 +0900

    quiz05 complete

--- pythonWorkspace/practice.py
+++ pythonWorkspace/practice.py
@@ -277,3 +277,81 @@
 # print( menu, type(menu) )
 # menu = set( menu )
 # print( menu, type(menu) )
+
+# # 분기( 조건문 )
+# # if() 함수
+# # if 조건 :
+# #   실행명령문
+# wether = "비"
+# wether = input("오늘 날씨는 어때요?")
+# if wether == "비" or wether == "눈":
+#     print("우산을 챙기세요.")
+# elif wether == "미세먼지" :
+#     print("마스크를 챙기세요.")
+# else :
+#     print("준비물 필요 없어요.")
+
+# if wether == "비" : print("우산을 채기세요")
+
+# tmp = int( input("기온은 어때요?") )
+# if 30 <= tmp : print("너무 더워요. 나가지 마세요.")
+# elif 10 <= tmp and tmp < 30 : print("괜찮은 날씨에요.")
+# elif 0 <= tmp < 10 : print("외투를 챙기세요.")
+# else : print("너무 추워요. 나가지 마세요.")
+
+# # 반복문
+# 1.for
+# for waiting_no in [0,1,2,3,4] : 
+#     print("대기번호 : {0}".format(waiting_no));
+# for waiting_no in range(5) : 
+#     print("대기번호 : {0}".format(waiting_no))
+
+# starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+# for customer in starbucks : 
+#     print("{0}손님 주문하신 커피 준비되었습니다.".format(customer))
+
+# 2.while : 조건이 만족할 때까지 반복
+# customer = "토르"
+# index = 5
+# while index >= 1 :
+#     print("{0}, 커피가 준비되었습니다. {1}번 남았어요.".format(customer, index))
+#     index -= 1
+#     if index == 0 : print("커피는 폐기처분 되었습니다.")
+
+# customer = "아이언맨"
+# index = 0
+# while True :
+#     print("{0}, 커피가 준비되었습니다. 호출 {1}번".format(customer, index))
+#     index += 1
+
+# customer = "토르"
+# person = "Unkown"
+# while person != customer : 
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+#     person = input("이름이 어떻게 되세요?")
+
+# continue 와 break
+# absent = [2,5] # 결석
+# no_book = [7] # 책을 안가져옴
+# for student in range(1,10) : 
+#     if student in absent : continue # 해당 반복 차례를 넘김
+#     if student in no_book : 
+#         print("오늘 수업은 여기까지. {0}은(는) 교무실로 따라와 ~!!!".format(student))
+#         break
+#     print("{0}야, 책을읽어봐!!".format(student))
+
+# # 한 줄 for
+# # 출석번호 1 2 3 4, 앞에 100을 붙이기로함 -> 101, 102, 103, 104...
+# student = [1,2,3,4,5]
+# print(student)
+# student = [i + 100 for i in student]
+# print(student)
+
+# # 학생 이름을 길이로 변환
+# student = ["Iron man", "Thor", "I an groot"]
+# print(student)
+# student = [len(i) for i in student]
+# print(student)
+student = ["Iron man", "Thor", "I an groot"]
+student = [i.upper() for i in student]
+print(student)
