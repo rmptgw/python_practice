60ca4db01316cee2f7b0b742fd2c6c74ec59a5a0
rmptgw <rmptgw@gmail.com>
2021-02-16 22:45:51 +0900

    클래스 및 생성자

--- pythonWorkspace/practice.py
+++ pythonWorkspace/practice.py
@@ -505,3 +505,59 @@
 # 	study_file.write('파이썬을 열심히 공부하고 있어요.')
 # with open('study.txt', 'r', encoding='utf8') as study_file:
 # 	print(study_file.read())
+
+# # class : 서로 연관이 있는 변수와 함수의 집합
+# # 마린: 공격 유닛, 군인. 총을 쏠 수 있음
+# name = '마린' # 유닛 이름
+# hp = 40 # 유닛 체력
+# damage = 5 # 유닛 공격력
+
+# print('{0} 유닛이 생성되었습니다.'.format(name))
+# print('체력 {0}, 공격력 {1} \n'.format(hp, damage))
+
+# # 탱크 : 공격 유닛, 탱크. 포를 쏠 수 있는데, 일반 모드 / 시즈 모드.
+# tank_name = '탱크'
+# tank_hp = 150
+# tank_damage = 35
+# print('{0} 유닛이 생성되었습니다.'.format(tank_name))
+# print('체력 {0}, 공격력 {1} \n'.format(tank_hp, tank_damage))
+
+# tank2_name = '탱크'
+# tank2_hp = 150
+# tank2_damage = 35
+# print('{0} 유닛이 생성되었습니다.'.format(tank2_name))
+# print('체력 {0}, 공격력 {1} \n'.format(tank2_hp, tank2_damage))
+
+# def attack(name, location, damage):
+# 	print('{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]'.format(\
+# 		name, location, damage))
+
+# attack(name, '1시', damage)
+# attack(tank_name, '1시', tank_damage)
+# attack(tank2_name, '1시', tank2_damage)
+# # __init__ : 생성자
+# # 맴버 변수 : class 내에서 선언된 변수 
+# # ex)
+# # self.name에서 .name, 
+# # self.hp 에서 .hp
+# class Unit:
+# 	def __init__(self, name, hp, damage):
+# 		self.name = name
+# 		self.hp = hp
+# 		self.damage = damage
+# 		print('{0} 유닛이 생성되었습니다.'.format(self.name))
+# 		print('체력 {0}, 공격력 {1}'.format(self.hp, self.damage))
+
+# marine1 = Unit('마린', 40, 5)
+# marine2 = Unit('마린', 40, 5)
+# tank = Unit('탱크', 150, 35)
+
+# wraith1 = Unit('레이스', 80, 5)
+# print('유닛 이름 : {0}, 공격력 : {1}'.format(wraith1.name, wraith1.damage))
+
+# wraith2 = Unit('레이스', 80, 5)
+# wraith2.clocking = True
+
+# if wraith2.clocking == True:
+# 	print('{0}는 현재 클록킹 상태입니다.'.format(wraith2.name))
+
