b3c0d77d63e073ea99d77dfb6891643aa6e5d336
rmptgw <rmptgw@gmail.com>
2021-02-22 11:08:58 +0900

    다중 상속 끝

--- practice.py
+++ practice.py
@@ -561,3 +561,76 @@
 # if wraith2.clocking == True:
 # 	print('{0}는 현재 클록킹 상태입니다.'.format(wraith2.name))
 
+# class Unit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print("{0} 유닛이 생성되었습니다.".format(self.name))
+#         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+# class AttackUnit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]"\
+#         .format(self.name, location, self.damage))
+
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
+#         if( self.hp <= 0 ):
+#             print("{0} : 파괴되었습니다.".format(self.name))
+
+# # 파이어뱃 : 공격유닛, 화염방사기
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+
+# # 공격 2번 받는다고 가정
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+
+class Unit: # 일반유닛
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]"\
+        .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
+        if( self.hp <= 0 ):
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+# 드랍쉽 : 공중 유닛, 수송기, 마린/ 파이어뱃/ 탱크 등을 수송
+# 날 수 있는 기능을 가진 클래스
+class Flyable: 
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도{2}]"\
+            .format(name, location, self.flying_speed))
+
+class FlyableAttackUnit( AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp,damage)
+        Flyable.__init__(self, flying_speed)
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
+
