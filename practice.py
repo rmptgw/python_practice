77984af2ec20aaebb0a4f77cc49909073b5eded4
rmptgw <rmptgw@gmail.com>
2021-03-02 21:18:21 +0900

    complete for quiz 9

--- practice.py
+++ practice.py
@@ -828,3 +828,61 @@
 # # 게임 종료
 # game_over()
 
+# try : 
+#     print('나누기 전용 계산기입니다.')
+#     nums = []
+#     nums.append(int(input('첫 번째 숫자를 입력하세요 : ')))
+#     nums.append(int(input('두 번째 숫자를 입력하세요 : ')))
+#     # nums.append(int(nums[0] / nums[1]))int(num1/num2)
+#     print('{0} / {1} = {2}'.format(nums[0], nums[1], nums[2]))
+# except ValueError :
+#     print('에러! 잘못된 값을 입력하였습니다.')
+# except ZeroDivisionError as err :
+#     print(err)
+# except Exception as err :
+#     print("알 수 없는 에러가 발생하였습니다.")
+#     print(err)
+
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+
+#     def __str__(self) :
+#         return self.msg
+
+# try :
+#     print('한 자리 숫자 나누기 전용 계산기입니다.')
+#     num1 = int(input('첫 번째 숫자를 입력하세요 : '))
+#     num2 = int(input('두 번째 숫자를 입력하세요 : '))
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError('입력값 : {0}, {1}'.format(num1, num2))
+#     print('{0} / {1} = {2}'.format(num1, num2, int(num1 / num2)))
+# except BigNumberError as err:
+#     print('에러가 발생하였습니다.. 한 자리 숫자만 입력하세요.')
+#     print(err)
+# finally:
+#     print('계산기를 이용해 주셔서 감사합니다.')
+
+# import theater_module as mv # as 를 통해 별칭을 사용할 수 있다.
+
+# theater_module.price(3) # 3명이서 영화보러 갔을 때
+# theater_module.price_morning(4) # 4명이서 조조할인 영화보러 갔을 때
+# theater_module.price_soldier(5) # 5명의 군인이 영화보러 갔을 때
+
+# mv.price(3) # 3명이서 영화보러 갔을 때
+# mv.price_morning(4) # 4명이서 조조할인 영화보러 갔을 때
+# mv.price_soldier(5) # 5명의 군인이 영화보러 갔을 때
+
+# from theater_module import *
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+# # 부분 로드
+# from theater_module import price, price_morning
+# price(3)
+# price_morning(4)
+
+# # 함수 별칭 사용
+# from theater_module import price_soldier as price
+# price(3)
