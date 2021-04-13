e35bb16f8ab0d5afde3d468dc32a7d11d8417933
rmptgw <rmptgw@gmail.com>
2021-04-13 21:48:15 +0900

    finishing the ball movement

--- pygame_project/2_weapon_keyevent.py
+++ pygame_project/2_weapon_keyevent.py
@@ -57,6 +57,18 @@ character_height = character_size[1]
 character_x_pos  = ( screen_width  / 2 ) - ( character_width  / 2 )
 character_y_pos  = screen_height - character_height - stage_height
 
+
+# 무기 설정
+weapon     = pygame.image.load( os.path.join(image_path,"weapon.png") )
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+# 무기는 여러발 발사 가능
+weapons = []
+
+# 무기 속도 설정
+weapon_speed = 10
+
+
 balloon1   = pygame.image.load( os.path.join(image_path, "balloon1.png") )
 balloon2   = pygame.image.load( os.path.join(image_path, "balloon2.png") )
 balloon3   = pygame.image.load( os.path.join(image_path, "balloon3.png") )
@@ -86,12 +98,25 @@ while running:
                 character_to_x -= character_speed
             elif event.key == pygame.K_RIGHT:  # 방향키 오른쪽 / 우측으로 이동
                 character_to_x += character_speed
+            elif event.key == pygame.K_SPACE: # 스페이스 / 무기발사
+                weapon_x_pos = character_x_pos + ( character_width / 2 ) - ( weapon_width / 2 )
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+
+
             
         if event.type == pygame.KEYUP: # 키보드가 눌리지 않으면
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 character_to_x = 0
+
     
     # 3. 게임 캐릭터 위치 정의
+    # 무기 위치 조정
+    # 무기 발사시 x값은 변동 없고 y값은 변동됨
+    weapons = [ [ w[0], w[1] - weapon_speed ] for w in weapons ]
+    
+    # 천장에 닿은 무기 없애기
+    weapons = [ [ w[0], w[1] ] for w in weapons if w[1] > 0 ]
     
     # dt를 곱함으로 프레임에 관계없이 속도 보정을 해줌
     character_x_pos += character_to_x * dt 
@@ -123,6 +148,10 @@ while running:
 
     # 5. 화면에 그리기
     screen.blit( background, (0,0) ) # 배경화면 구현
+
+    # 무기 구현
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
 
     screen.blit( stage,      ( 0, screen_height - stage_height ) )  # 스테이지 구현
     screen.blit( character,  ( character_x_pos, character_y_pos ) )  # 캐릭터 구현
--- pygame_project/3_ball_movement.py
+++ pygame_project/3_ball_movement.py
@@ -56,25 +56,46 @@ character_width  = character_size[0]
 character_height = character_size[1]
 character_x_pos  = ( screen_width  / 2 ) - ( character_width  / 2 )
 character_y_pos  = screen_height - character_height - stage_height
-
-balloon1   = pygame.image.load( os.path.join(image_path, "balloon1.png") )
-balloon2   = pygame.image.load( os.path.join(image_path, "balloon2.png") )
-balloon3   = pygame.image.load( os.path.join(image_path, "balloon3.png") )
-balloon4   = pygame.image.load( os.path.join(image_path, "balloon4.png") )
-
-
-# 이동할 좌표 설정
+# 캐릭터 이동 좌표 설정
 character_to_x = 0
 character_to_y = 0
-
-# 이동 속도 설정
+# 캐릭터 이동 속도 설정
 character_speed = 0.5
+
+# 무기 설정
+weapon     = pygame.image.load( os.path.join(image_path,"weapon.png") )
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+# 무기는 여러발 발사 가능
+weapons = []
+# 무기 속도 설정
+weapon_speed = 10
+
+# 공 만들기(4개 크기에 대해 따로 처리)
+ball_images = [
+    pygame.image.load( os.path.join(image_path, "balloon1.png") ),
+    pygame.image.load( os.path.join(image_path, "balloon2.png") ),
+    pygame.image.load( os.path.join(image_path, "balloon3.png") ),
+    pygame.image.load( os.path.join(image_path, "balloon4.png") )
+]
+# 공 크기에 따른 최초 속도
+ball_speed_y = [-18, -15, -12, -9] # index 0, 1, 2, 3 에 해당하는 값
+# 공들
+balls = []
+balls.append({
+    "pos_x" : 50,   # 공의 x좌표
+    "pos_y" : 50,   # 공의 y좌표
+    "img_idx" : 0,  # 공의 이미지 인덱스
+    "to_x" : 3,     # x축 이동 방향, -는 좌측, +는 우측
+    "to_y" : -6,    # y축 이동 방향
+    "init_spd_y" : ball_speed_y[0] # y 최초 속도
+})
 
 # 이벤트 루프
 running = True # 게임이 진행중인가?
 while running:
     dt = clock.tick(30) # 게임 화면의 초당 프레임 수 설정
-    # print("fps : " + str(clock.get_fps()))
+    print("fps : " + str(clock.get_fps()))
 
     # 2. 이벤트 처리( 키보드, 마우스 등 )
     for event in pygame.event.get():  # 사용자의 입력을 감지
@@ -86,13 +107,29 @@ while running:
                 character_to_x -= character_speed
             elif event.key == pygame.K_RIGHT:  # 방향키 오른쪽 / 우측으로 이동
                 character_to_x += character_speed
+            elif event.key == pygame.K_SPACE: # 스페이스 / 무기발사
+                weapon_x_pos = character_x_pos + ( character_width / 2 ) - ( weapon_width / 2 )
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+
+
             
         if event.type == pygame.KEYUP: # 키보드가 눌리지 않으면
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 character_to_x = 0
+
     
     # 3. 게임 캐릭터 위치 정의
+    # 무기 위치 조정
+    # 무기 발사시 x값은 변동 없고 y값은 변동됨
+    weapons = [ [ w[0], w[1] - weapon_speed ] for w in weapons ]
+    # 천장에 닿은 무기 없애기
+    weapons = [ [ w[0], w[1] ] for w in weapons if w[1] > 0 ]
     
+    # 공 위치 정의
+    # for ball_idx, ball_val in enumerate(balls);
+
+
     # dt를 곱함으로 프레임에 관계없이 속도 보정을 해줌
     character_x_pos += character_to_x * dt 
     character_y_pos += character_to_y * dt
@@ -123,6 +160,10 @@ while running:
 
     # 5. 화면에 그리기
     screen.blit( background, (0,0) ) # 배경화면 구현
+
+    # 무기 구현
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
 
     screen.blit( stage,      ( 0, screen_height - stage_height ) )  # 스테이지 구현
     screen.blit( character,  ( character_x_pos, character_y_pos ) )  # 캐릭터 구현
